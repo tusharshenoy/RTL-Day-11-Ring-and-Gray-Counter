a89df310aa28e02181bfe0c41792dd4e4c9ce424
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-09-30 19:53:34 +0530

    Update RingCounter3Bit.v

--- RingCounter3Bit.v
+++ RingCounter3Bit.v
@@ -1,10 +1,9 @@
-module RingCounter3Bit(clk, reset,set, Q);
+module RingCounter3Bit(clk, reset,preset, Q);
 
-input clk,reset,set; 
+input clk,reset,preset; 
 output [2:0]Q; 
 
-D_Flip_Flop DFF_0(.D(Q[2]+set), .reset(reset), .clk(clk), .Q(Q[0]));
+D_Flip_Flop DFF_0(.D(Q[2]+preset), .reset(reset), .clk(clk), .Q(Q[0]));
 D_Flip_Flop DFF_1(.D(Q[0]), .reset(reset), .clk(clk), .Q(Q[1])); 
 D_Flip_Flop DFF_2(.D(Q[1]), .reset(reset), .clk(clk), .Q(Q[2])); 
-
 endmodule
