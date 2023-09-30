5e234b8fb057a08c97cd61ed41668970decb5fd2
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-09-30 19:44:00 +0530

    Update RingCounter3Bit_tb.v

--- RingCounter3Bit_tb.v
+++ RingCounter3Bit_tb.v
@@ -1,16 +1,16 @@
 module RingCounter3Bit_tb;
 
-reg clk,reset,preset;
+reg clk,reset,set;
 wire [2:0]Q;
 
-RingCounter3Bit dut(clk, reset,preset, Q);
+RingCounter3Bit dut(clk, reset,set,Q);
 
 initial begin
    clk=1'b0;
    reset=1'b1;
-   preset=1'b1;
+   set=1'b1;
    #8 reset=1'b0;
-   #8 preset=1'b0;
+   #8 set=1'b0;
 end
 
 always #5 clk=~clk;
