7f4836f42bf7962a30c14cd0c633cf49dcf736ce
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-09-30 19:36:25 +0530

    Create RingCounter3Bit_tb.v

---
+++ RingCounter3Bit_tb.v
@@ -0,0 +1,18 @@
+module RingCounter3Bit_tb;
+
+reg clk,reset,preset;
+wire [2:0]Q;
+
+RingCounter3Bit dut(clk, reset,preset, Q);
+
+initial begin
+   clk=1'b0;
+   reset=1'b1;
+   preset=1'b1;
+   #8 reset=1'b0;
+   #8 preset=1'b0;
+end
+
+always #5 clk=~clk;
+
+endmodule
